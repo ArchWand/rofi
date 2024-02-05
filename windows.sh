a0886f60080837c7508282866809d7d85736803a
Robert Zhu <arcwandx86@gmail.com>
2024-02-04 22:05:56 -0500

    Polish alt-tab menu

--- windows.sh
+++ windows.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rofi -show window -monitor -1 -theme "~/.config/rofi/themes/catppuccin-mocha.rasi" -theme-str "$(cat ~/.config/rofi/themes/window_diff.rasi)" -kb-accept-entry '!Alt-Tab,!Alt-ISO_Left_Tab,!Alt-Alt_L,!Alt_L,!Alt-Alt_R,!Alt_R' -kb-row-down Alt-Tab -kb-row-down Alt-Tab -kb-row-up Alt-ISO_Left_Tab
+rofi -show window -monitor -1 -theme "~/.config/rofi/themes/catppuccin-mocha.rasi" -theme-str "$(cat ~/.config/rofi/themes/window_diff.rasi)" -kb-accept-entry '!Alt-Tab,!Alt-ISO_Left_Tab,!Alt-Alt_L,!Alt_L,!Alt-Alt_R,!Alt_R' -kb-row-down Alt-Tab -kb-row-up Alt-ISO_Left_Tab
